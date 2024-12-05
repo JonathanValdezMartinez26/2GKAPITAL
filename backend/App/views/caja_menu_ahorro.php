716e7f2916dd7295bee420f043d06d9575893fa3
Jon <jvaldez_2610@hotmail.com>
2024-12-05 15:18:54 -0600

    Cambio 1

--- backend/App/views/caja_menu_ahorro.php
+++ backend/App/views/caja_menu_ahorro.php
@@ -17,20 +17,12 @@ $buscarCliente = new BuscarCliente('Para realizar un movimiento es necesario que
             <a id="link" href="/Ahorro/CuentaCorriente/">
                 <div class="col-md-5" style="margin-top: 5px; margin-left: 10px; margin-right: 30px; border: 1px solid #dfdfdf; border-radius: 10px;">
                     <img src="https://cdn-icons-png.flaticon.com/512/5575/5575939.png" style="border-radius: 3px; padding-top: 5px;" width="110" height="110">
-                    <p style="font-size: 12px; padding-top: 5px; color: #000000"><b>Ahorro </b></p>
+                    <p style="font-size: 12px; padding-top: 5px; color: #000000"><b>Ahorro y Crédito </b></p>
                     <! -- https://cdn-icons-png.flaticon.com/512/5575/5575938.png -->
                 </div>
             </a>
 
 
-            <a id="link" href="/Ahorro/ContratoInversion/">
-                <div class="col-md-5 imagen" style="margin-top: 5px; margin-left: 0px; border: 1px solid #dfdfdf; border-radius: 10px;">
-                    <img src="https://cdn-icons-png.flaticon.com/512/5836/5836503.png" style="border-radius: 3px; padding-top: 5px;" width="110" height="110">
-                    <p style="font-size: 12px; padding-top: 5px; color: #000000"><b>Inversión </b></p>
-                    <! -- https://cdn-icons-png.flaticon.com/512/5836/5836477.png -->
-                </div>
-            </a>
-            
             <a id="link" href="/Ahorro/ContratoInversion/">
                 <div class="col-md-5 imagen" style="margin-top: 5px; margin-left: 0px; border: 1px solid #dfdfdf; border-radius: 10px;">
                     <img src="https://cdn-icons-png.flaticon.com/512/5836/5836503.png" style="border-radius: 3px; padding-top: 5px;" width="110" height="110">
